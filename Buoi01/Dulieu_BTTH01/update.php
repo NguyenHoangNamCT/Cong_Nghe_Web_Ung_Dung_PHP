15fd1ac2813f46364937c18c32c68372d2dcfa4c
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2022-10-30 15:59:10 +0700

    Xen lại

--- Buoi01/Dulieu_BTTH01/update.php
+++ Buoi01/Dulieu_BTTH01/update.php
@@ -1,3 +1,34 @@
+<?php //Tại sao biến id khi bấm sửa lại thành biến không xác định? Mỗi lần load lại mọi biến đều thành không xd sao?
+
+    // $link = mysqli_connect('localhost', 'root', 'vertrigo');
+    // mysqli_select_db($link, 'qlsv_cnw');
+    // //Thực hiện truy vấn cho bảng lớp
+    // $resultsL = mysqli_query($link, 'select * from lop');
+
+    // // if(!isset($_GET['id']))// lúc bấm sửa nó vẫn chạy dô đây được
+    // //   return;
+
+    // if(isset($_GET['id']))
+    //   $id = $_GET['id'];
+
+    // if(isset($_POST['nameMSSV']) && isset($_POST['nameHoTen']) && isset($_POST['nameEmail']) && isset($_POST['nameLop'])){
+    //     // $id = $_POST['nameID'];
+    //     $mssv = $_POST['nameMSSV'];
+    //     $hoten = $_POST['nameHoTen'];
+    //     $email = $_POST['nameEmail'];
+    //     $lop = $_POST['nameLop'];
+    //     //$sTruyVan = "UPDATE sinhvien set mssv = '".$_POST['nameMSSV']."', hoten = '".$_POST['nameHoTen']."', email = '".$_POST['nameEmail']."', lopid = ".$_POST['nameLop']." where id = ".$_GET['id']."";//làm kiểu củ chuổi này mất time mà lỗi không biết đường fix :D
+    //     $sTruyVan = "UPDATE sinhvien set mssv ='$mssv', hoten = '$hoten', email = '$email', lopid = '$lop' where id = $id";        
+    //     if(mysqli_query($link, $sTruyVan))
+    //       header("location:index.php");
+    // }
+    // else{
+    //   $sTruyVan = 'select * from sinhvien where id='.$id;
+    //   $sv = mysqli_fetch_array($resultsSV = mysqli_query($link, $sTruyVan));//vì có 1 dòng nên làm vậY luôn cho nhanh
+    //   echo var_dump($sv);
+    // }
+?>
+
 <?php
     $link = mysqli_connect('localhost', 'root', 'vertrigo');
     mysqli_select_db($link, 'qlsv_cnw');
@@ -10,16 +41,16 @@
         $hoten = $_POST['nameHoTen'];
         $email = $_POST['nameEmail'];
         $lop = $_POST['nameLop'];
-        //$id = $_GET['id'];
         //$sTruyVan = "UPDATE sinhvien set mssv = '".$_POST['nameMSSV']."', hoten = '".$_POST['nameHoTen']."', email = '".$_POST['nameEmail']."', lopid = ".$_POST['nameLop']." where id = ".$_GET['id']."";//làm kiểu củ chuổi này mất time mà lỗi không biết đường fix :D
         $sTruyVan = "UPDATE sinhvien set mssv ='$mssv', hoten = '$hoten', email = '$email', lopid = '$lop' where id = $id";        
-        if(mysqli_query($link, $sTruyVan))
+        if(mysqli_query($link, $sTruyVan)){
           header("location:index.php");
-        var_dump($id);
+        }
     }
     else{
       $sTruyVan = 'select * from sinhvien where id='.$_GET['id'];
       $sv = mysqli_fetch_array($resultsSV = mysqli_query($link, $sTruyVan));//vì có 1 dòng nên làm vậY luôn cho nhanh
+      echo var_dump($sv);
     }
 ?>
 <!DOCTYPE html>
@@ -34,8 +65,12 @@
 <body>
 <div class="container">
   <h1>Sửa sinh viên</h1>
+  <!-- <?php
+    var_dump($id);
+    var_dump($sv);
+  ?> -->
   <form action="update.php" method="post">
-    <input type="hidden" name="nameID"  value="<?php echo $sv['id']; ?>">
+    <input type="hidden" name="nameID"  value="<?php echo $sv['id'];?>"> 
     <div class="form-group">
       <label>MSSV:</label>
       <input class="form-control" type="text" id="idMSSV" name="nameMSSV" value="<?php echo $sv['mssv']; ?>">
